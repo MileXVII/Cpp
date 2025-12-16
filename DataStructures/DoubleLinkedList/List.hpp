0793d4b4ee875344cfc9a5216cfa776ace692a68
Michael Raneri <RNRMHL05P05L042I@studium.unict.it>
2025-12-16 17:23:18 +0100

    Refactor List class constructor and destructor

--- DataStructures/DoubleLinkedList/List.hpp
+++ DataStructures/DoubleLinkedList/List.hpp
@@ -8,19 +8,58 @@
 template <typename T> class List {
 public:
 
-    List();
-    bool isEmpty() const;
+    List() : size(0) {
+        head = new node_s;
+        head->next = head;
+        head->prev = head;
+        head->sentinel = true;
+    }
+
+    bool isEmpty() const { return (size == 0); }
+    int getSize() const { return size; }
+
+    void printList() const {
+        if(!head) return;
+        node_s *c = head->next;
+        while(c != head) {
+            std::cout << "[" << c->value << "]-->";
+            c = c->next;
+        }
+        std::cout << std::endl;
+    }
+
+    void printMemoryDump() const {
+        std::cout << "\n=============== Memory Dump ===============\n";
+
+        node_s *c = head;
+        do {
+        std::cout << "\nNode @ " << static_cast<void*>(c);
+        if(c == head) {std::cout << "   [SENTINEL]\n"; } else { std::cout << std::endl; }
+        std::cout << "  prev: " << static_cast<void*>(c->prev) << ((c->prev == head) ? "   (head)" : "") << "\n";
+        std::cout << "  next: " << static_cast<void*>(c->next) << ((c->next == head) ? "   (head)" : "") << "\n";
+        if(c != head) { std::cout << "  value: " << c->value << "\n\n"; }
+        
+
+        c = c->next;
+        }while(c != head);
+        std::cout << "===========================================\n";
+    }
+
+    ~List() {
+        node_s *c = head->next;
+        while(c != head) {
+            node_s *tmp = c;
+            c = c->next;
+            delete tmp;
+        }
+        delete head;
+        head = nullptr;
+    }
+
     void push_front(T ele);
     void push_back(T ele);
     T pop_front();
     T pop_back();
-
-    int getSize() const;
-
-    void printList() const;
-    void printMemoryDump() const;
-
-    ~List();
 
 private:
     typedef struct node {
@@ -33,6 +72,7 @@ private:
     int size;  
 };
 
-#include "List.tpp"
+// Push and Pop related functions template 
+#include "List.cpp"
 
 #endif // LIST_HPP
